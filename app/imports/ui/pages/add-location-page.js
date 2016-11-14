ab3edd4ea7eadf4c9be4eaf7a71e81f851345541
Steven-Braun <Steven-Braun@users.noreply.github.com>
2016-11-14 02:52:42 -1000

    Add files via upload

---
+++ app/imports/ui/pages/add-location-page.js
@@ -0,0 +1,24 @@
+import { Template } from 'meteor/templating';
+import { Places } from '../../api/recipes/places.js';
+import { _ } from 'meteor/underscore';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+Template.Add_Location_Page.helpers({
+    locations() {
+  return Places.find();
+},
+});
+
+Template.Add_Location_Page.onCreated(function onCreated() {
+  this.autorun(() => {
+    this.subscribe('Places');
+  });
+});
+
+Template.Add_Location_Page.events({});
+
+Template.Add_Location_Page.onRendered(function (){
+  this.$('.dropdown').dropdown({
+    allowAdditions: true,
+  });
+});
