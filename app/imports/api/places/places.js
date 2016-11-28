d9d312eab9fd54fd057088e8d6d71705dac190f2
Steven-Braun <Steven-Braun@users.noreply.github.com>
2016-11-28 02:30:00 -1000

    Places JS update

--- app/imports/api/places/places.js
+++ app/imports/api/places/places.js
@@ -4,3 +4,33 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 /* eslint-disable object-shorthand */
 
 export const Places = new Mongo.Collection('Places');
+
+/**
+ * Schema for Places
+ */
+export const PlacesSchema = new SimpleSchema({
+      placename: {
+        label: 'placename',
+        type: String,
+        optional: false,
+        max: 50,
+      },
+      distance: {
+        label: 'cost',
+        type: Number,
+        optional: false,
+      },
+      ingredients: {
+        label: 'ingredients',
+        type: [String],
+        optional: false,
+      },
+      image: {
+        label: 'image',
+        type: String,
+        optional: false,
+      },
+    },
+);
+
+Places.attachSchema(PlacesSchema);
