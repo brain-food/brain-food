5243d98f6d0c8f49141d8ac364a00228c9739659
Kea Uehara <keal@hawaii.edu>
2016-11-29 03:11:17 -1000

    Recipes can now be edited.
    Added an edit button on food item pages for owners of recipes which
    allows them to edit their recipe.

--- app/imports/ui/pages/food-item-page.js
+++ app/imports/ui/pages/food-item-page.js
@@ -11,11 +11,16 @@ Template.Food_Item_Page.onCreated(function onCreated() {
   });
 });
 
-
 Template.Food_Item_Page.helpers({
   recipeField(fieldName) {
     const recipe = Recipes.findOne(FlowRouter.getParam('_id'));
     // See https://dweldon.silvrback.com/guards to understand '&&' in next line.
     return recipe && recipe[fieldName];
+  },
+  recipeId() {
+    return FlowRouter.getParam('_id');
+  },
+  owner() {
+    return Meteor.user().profile.name === Recipes.findOne(FlowRouter.getParam('_id')).username;
   },
 });
