b6eab801fa5af454a58848233898e5d16fad3c28
RileyMiyamoto <rkam9@hawaii.edu>
2016-12-06 11:22:41 -1000

    account page changes

--- app/imports/ui/pages/account-page.js
+++ app/imports/ui/pages/account-page.js
@@ -18,6 +18,9 @@ Template.Account_Page.helpers({
     // See https://dweldon.silvrback.com/guards to understand '&&' in next line.
     return profile && profile[fieldName];
   },
+  profileList(){
+    return Profiles.find();
+  },
   owner() {
     return Meteor.user().profile.name === Profiles.findOne(FlowRouter.getParam('_id')).username;
   },
