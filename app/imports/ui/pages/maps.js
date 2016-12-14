fee5bb5df16f7f837a2c6cb46964e6c414f42c33
RileyMiyamoto <rkam9@hawaii.edu>
2016-12-14 01:08:56 -1000

    added markers to google maps

--- app/imports/ui/pages/maps.js
+++ app/imports/ui/pages/maps.js
@@ -1,15 +1,22 @@
-import { Template } from 'meteor/templating';
+import {Template} from 'meteor/templating';
 Markers = new Mongo.Collection('markers');
 
 if (Meteor.isClient) {
-  Template.Google_Maps.onCreated(function() {
-    GoogleMaps.ready('map', function(map) {
-      google.maps.event.addListener(map.instance, 'click', function(event) {
+  Template.Google_Maps.onCreated(function () {
+    GoogleMaps.ready('map', function (map) {
+      google.maps.event.addListener(map.instance, 'click', function (event) {
         Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
       });
 
       var markers = {};
-
+      new google.maps.Marker({
+        position: new google.maps.LatLng(21.3000, -157.8190),
+        map: map.instance
+      });
+      new google.maps.Marker({
+        position: new google.maps.LatLng(21.29221, -157.8237777),
+        map: map.instance
+      });
       Markers.find().observe({
         added: function (document) {
           var marker = new google.maps.Marker({
@@ -20,8 +27,8 @@ if (Meteor.isClient) {
             id: document._id
           });
 
-          google.maps.event.addListener(marker, 'dragend', function(event) {
-            Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+          google.maps.event.addListener(marker, 'dragend', function (event) {
+            Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() } });
           });
 
           markers[document._id] = marker;
@@ -38,27 +45,18 @@ if (Meteor.isClient) {
     });
   });
 
-  Meteor.startup(function() {
+  Meteor.startup(function () {
     GoogleMaps.load();
   });
 
   Template.Google_Maps.helpers({
-    mapOptions: function() {
+    mapOptions: function () {
       if (GoogleMaps.loaded()) {
         return {
           center: new google.maps.LatLng(21.3000, -157.8190),
           zoom: 15,
-          marker: new google.maps.LatLng(21.3000, -157.8190),
         };
       }
     },
-    mapOptions1: function() {
-      if (GoogleMaps.loaded()) {
-        return {
-          center: new google.maps.LatLng(21.3000, 157.8190),
-          zoom: 15
-        };
-      }
-    }
   });
 }
