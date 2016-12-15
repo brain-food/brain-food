7beabde86640539e5758a6532205cc9bf98b9b21
clint-morimoto <clintmor@hawaii.edu>
2016-12-14 18:59:45 -1000

    comments collection insertion in progress...

--- app/imports/ui/pages/place-page.js
+++ app/imports/ui/pages/place-page.js
@@ -1,11 +1,17 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Template } from 'meteor/templating';
 import { _ } from 'meteor/underscore';
-import { Comments } from '../../api/comments/comments.js';
+import { Comments, CommentSchema } from '../../api/comments/comments.js';
 
 /* eslint-disable object-shorthand, no-unused-vars */
 
+const displayErrorMessages = 'displayErrorMessages';
+
 Template.Place_Page.onCreated(function onCreated() {
+  this.messageFlags = new ReactiveDict();
+  this.messageFlags.set(displayErrorMessages, false);
+  this.context = CommentSchema.namedContext('Place_Page');
   this.autorun(() => {
     this.subscribe('Comments');
   });
@@ -23,6 +29,9 @@ Template.Place_Page.helpers({
   commentCount() {        // returns the number of comments in the list
     return Comments.find().count();
   },
+  errorClass() {
+    return Template.instance().messageFlags.get(displayErrorMessages) ? 'error' : '';
+  },
   displayFieldError(fieldName) {
     const errorKeys = Template.instance().context.invalidKeys();
     return _.find(errorKeys, (keyObj) => keyObj.name === fieldName);
@@ -30,7 +39,28 @@ Template.Place_Page.helpers({
 });
 
 Template.Place_Page.events({
-  'click .ui.rightB.inverted.comment.button': function() {
-    console.log("Hello Comment");
+  'submit .place-comment-form'(event, instance) {
+
+    event.preventDefault();
+
+    // Get fields and put them in variables
+    const comment = event.target.comment.value;
+
+    // Put all variables into one object
+    const newComment = { comment };
+
+    // Clear out any old validation errors.
+    instance.context.resetValidation();
+    // Invoke clean so that newRecipe reflects what will be inserted.
+    CommentSchema.clean(newComment);
+
+    // Determine validity.
+    instance.context.validate(newComment);
+    if (instance.context.isValid()) {
+      instance.messageFlags.set(displayErrorMessages, false);
+      let id = Comments.insert(newComment);
+    } else {
+      instance.messageFlags.set(displayErrorMessages, true);
+    }
   },
 });
